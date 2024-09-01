72d85fb89c787119d2b29f54e131e1ae8e5dd9f6
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-02 02:05:31 +0530

    removed the two asset because its hindrance

--- src/Components/Bubbles.jsx
+++ src/Components/Bubbles.jsx
@@ -5,9 +5,9 @@ const Bubbles = () => {
             <div className="sphere2"></div>
             <div className="sphere3"></div>
             <div className="sphere4"></div>
-            <img className='console' src='/console.png'></img>
+            {/* <img className='console' src='/console.png'></img> */}
             <img className='game' src='/console.png'></img>
-            <img className='captainamerica' src='/captainamerica.png'></img>
+            {/* <img className='captainamerica' src='/captainamerica.png'></img> */}
             <img className='Rectangle' src='/Rectangle.png'></img>
             <img className='coding' src='/coding.png'></img>
             <img className='badminton' src='/badminton.png'></img>
