5d01cd8f7f3158192a9c59e2e3483957da1e19ce
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-02 01:53:45 +0530

    in this file you have the assets

---
+++ src/Components/Bubbles.jsx
@@ -0,0 +1,18 @@
+const Bubbles = () => {
+    return (
+        <div>
+            <div className="sphere1"></div>
+            <div className="sphere2"></div>
+            <div className="sphere3"></div>
+            <div className="sphere4"></div>
+            <img className='console' src='/console.png'></img>
+            <img className='game' src='/console.png'></img>
+            <img className='captainamerica' src='/captainamerica.png'></img>
+            <img className='Rectangle' src='/Rectangle.png'></img>
+            <img className='coding' src='/coding.png'></img>
+            <img className='badminton' src='/badminton.png'></img>
+        </div>
+    )
+}
+
+export default Bubbles
