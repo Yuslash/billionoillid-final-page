dc66394663de85d2f4e7cc0540eb664bab4e158e
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-02 01:54:30 +0530

    this file is a experince which have all imporot function

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Bubbles from './Components/Bubbles'
+import Site from './Components/Site'
+
+function App() {
+
+  return (
+    <>
+      <Site />
+      <Bubbles />
+    </>
+  )
+}
+
+export default App
